1e6c3462f3617bc1fef4dd20de4aff19b0963bc6
mayahilbert <35238760+mayahilbert@users.noreply.github.com>
2025-02-27 20:26:53 -0500

    adding pages

--- main.js
+++ main.js
@@ -46,5 +46,16 @@ document.addEventListener('DOMContentLoaded', e => {
     //}
 });
 
-localStorage.setItem('hasVisited', true)
+//localStorage.setItem('hasVisited', true)
 
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
